9d1580e96ad086df8232ebcc2738ab20cc470d53
wishihab <wishihab@gmail.com>
2021-01-27 15:18:05 +0700

    tryin new splashscreen method by image/drawable

--- app/src/main/java/com/wishihab/mycontactapi/view/SplashScreen.kt
+++ app/src/main/java/com/wishihab/mycontactapi/view/SplashScreen.kt
@@ -2,8 +2,6 @@ package com.wishihab.mycontactapi.view
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.wishihab.mycontactapi.R
 
@@ -11,13 +9,16 @@ class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.splash_screen_activity)
-
-        //Handling Splash Screen
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000)
+//        splashscreen by layout and thread
+//        setContentView(R.layout.splash_screen_activity)
+//        //Handling Splash Screen
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            val intent = Intent(this,MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }, 3000)
+        //best practice to use splashscreen by image
+        startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+        finish()
     }
 }
